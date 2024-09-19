e65b542d1dedf698db4498b239557912fe7e275a
Akhil T P <33170852+akhilelamadu@users.noreply.github.com>
2024-09-19 20:33:59 +0530

    reduce CCN StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,11 +1,9 @@
-
 #ifndef STRINGCALCULATOR_H
 #define STRINGCALCULATOR_H
 
 #include <string>
 #include <vector>
 #include <stdexcept>
-#include <sstream>
 
 class StringCalculator {
 public:
@@ -13,9 +11,12 @@ public:
 
 private:
     std::vector<int> parseNumbers(const std::string& numbers, const std::string& delimiter);
-    int toInt(const std::string& number);
     std::string getCustomDelimiter(const std::string& input);
+    std::string getNumberString(const std::string& input);
+    int sumNumbers(const std::vector<int>& numbers);
     void validateNumbers(const std::vector<int>& numbers);
+    int toInt(const std::string& number);
+    std::vector<int> filterLargeNumbers(const std::vector<int>& numbers);
 };
 
 #endif
