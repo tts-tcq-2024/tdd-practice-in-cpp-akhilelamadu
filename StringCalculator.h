8ce07ecbac5b963893bb2c1617f7bb6fe2abf997
Akhil T P <33170852+akhilelamadu@users.noreply.github.com>
2024-09-19 20:30:14 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,1 +1,21 @@
 
+#ifndef STRINGCALCULATOR_H
+#define STRINGCALCULATOR_H
+
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <sstream>
+
+class StringCalculator {
+public:
+    int add(const std::string& numbers);
+
+private:
+    std::vector<int> parseNumbers(const std::string& numbers, const std::string& delimiter);
+    int toInt(const std::string& number);
+    std::string getCustomDelimiter(const std::string& input);
+    void validateNumbers(const std::vector<int>& numbers);
+};
+
+#endif
